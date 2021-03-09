5e7c309201ebb799a423c4c04d5d4e370323842e
Everton Souza <everton@campussolucoes.com>
2021-03-09 08:35:11 -0300

    geral

---
+++ SalesWebMvc/SalesWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public string Message { get; internal set; }
+    }
+}
